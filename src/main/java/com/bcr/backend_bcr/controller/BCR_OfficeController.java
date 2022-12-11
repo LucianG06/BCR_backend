7356c1e8d58007283d48f6d30f98e26556866bbf
Lucian <luciangeorge06@yahoo.com>
2022-12-11 02:31:15 +0200

    CrossOrigin added

--- src/main/java/com/bcr/backend_bcr/controller/BCR_OfficeController.java
+++ src/main/java/com/bcr/backend_bcr/controller/BCR_OfficeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BCR_OfficeController {
 
     private final BCR_OfficeService service;
